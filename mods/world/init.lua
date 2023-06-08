3e469981aa2d037ac6cf48a0cb9f6206b331716f
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-06-08 14:00:39 -0400

    More stuffs

--- mods/world/init.lua
+++ mods/world/init.lua
@@ -1,3 +1,12 @@
 minetest.set_mapgen_setting("caves", "false", true)
 minetest.set_mapgen_setting("dungeons", "false", true)
 
+minetest.register_node("world:grass", {
+    tiles = {"dz_grass.png"}
+})
+minetest.register_node("world:stone", {
+    tiles = {"dz_stone.png"}
+})
+minetest.register_node("world:dirt", {
+    tiles = {"dz_dirt.png"}
+})
